b2b1772781846bd820562da2927d4fb62f79b3e2
Charles Shih <shichen120@gmail.com>
2017-12-22 18:13:03 +0800

    Add GenerateTestReport.py and its samples.

---
+++ block/GenerateTestReport.py
@@ -0,0 +1,7 @@
+# What to do next?
+# - make a cpu of office and enjoy it.
+# - make fio generates the results in normal and json+.
+# - get the json parts from the output files.
+# - analyse it and keep KPIs in python dict.
+# - load the dicts into PrettyTalbe.
+# - show it or export as csv files.
