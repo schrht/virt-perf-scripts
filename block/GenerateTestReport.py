25562aa0ce769887dd8d87819bf612811e9ab690
Charles Shih <shichen120@gmail.com>
2017-12-25 13:53:20 +0800

    Implement function file_to_raw().

--- block/GenerateTestReport.py
+++ block/GenerateTestReport.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # What to do next?
 # - make a cpu of office and enjoy it.
 # - make fio generates the results in normal and json+.
@@ -5,3 +7,89 @@
 # - analyse it and keep KPIs in python dict.
 # - load the dicts into PrettyTalbe.
 # - show it or export as csv files.
+
+import json
+
+
+class FioPerformanceKPIs():
+    '''
+    Get, deal with and covert the performance KPI data from FIO tools.
+    '''
+
+    # The list of raw data, the item is loaded from fio output file.
+    # Each item is a full data source (raw data) and it is in json format.
+    raw_data_list = []
+
+    # The list of performance KPIs, which are extracted from the raw data.
+    # Each item represents a single fio test and it is in python dict format.
+    perf_kpi_list = []
+
+    def file_to_raw(self, params):
+        '''
+        This function open a specified fio output file and read the first json block which is expected to be the fio outputs in json/json+ format.
+        And convert the json block into the json format in python. With the help of function byteify, it converts the unicode string to bytes.
+        '''
+
+        def byteify(inputs):
+            '''Convert unicode to utf-8 string.'''
+            if isinstance(inputs, dict):
+                return {
+                    byteify(key): byteify(value)
+                    for key, value in inputs.iteritems()
+                }
+            elif isinstance(inputs, list):
+                return [byteify(element) for element in inputs]
+            elif isinstance(inputs, unicode):
+                return inputs.encode('utf-8')
+            else:
+                return inputs
+
+        if 'data_file' not in params:
+            print 'Missing required params: params[data_file]'
+            return (1, )
+
+        try:
+            with open(params['data_file'], 'r') as json_file:
+                json_data = json.load(json_file)
+                raw_data = byteify(json_data)
+        except Exception, err:
+            print 'Error while handling data file: %s' % err
+            return (1, )
+
+        return (0, raw_data)
+
+    def load_raw_data(self, params):
+        '''
+        This function loads json raw data from a sort of fio output files and save them into self.raw_data_list.
+        '''
+        pass
+
+    def raw_to_kpi(self, params):
+        '''
+        This function extracts performance KPIs from a tuple of raw data.
+        '''
+        pass
+
+    def extracts_perf_kpis(self, params):
+        '''
+        This function extracts performance KPIs from self.raw_data_list and save the tuples into self.perf_kpi_list.
+        '''
+        pass
+
+
+if __name__ == '__main__':
+
+    perf_kpis = FioPerformanceKPIs()
+
+    params = {
+        'data_file':
+        '/home/cheshi/workspace/vsc_workspace/virt-perf-scripts/block/samples/1.sample'
+    }
+    (result, raw_data) = perf_kpis.file_to_raw(params)
+
+    if result == 0:
+        print raw_data
+    else:
+        print 'error!'
+
+    exit(0)
