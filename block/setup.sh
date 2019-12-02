de3a8f364a2184a13c140257c67391ad558a104a
Charles Shih <schrht@gmail.com>
2019-12-02 11:22:48 +0800

    update setup.sh

--- block/setup.sh
+++ block/setup.sh
@@ -1,11 +1,23 @@
-#!/bin/bash
-project=`cat /etc/redhat-release|grep -Po 'release \K[0-9]*'`
+#!/usr/bin/env bash
+
+# Description:
+#   Setup environment for running on RHEL.
+#
+# History:
+#   v1.0  2019-12-02  yuxin.sun  init version
+
+# Get system info
+project=$(cat /etc/redhat-release | grep -Po 'release \K[0-9]*')
 echo "Setup block test environment in RHEL-$project..."
+
+# Install fio
 yum install -y libaio-devel fio
-if [[ x$project == x'7' ]];then
+
+# Install Python runtime
+if [[ x$project == x'7' ]]; then
     yum install -y python python-yaml
     pip install click pandas numpy scipy
-elif [[ x$project == x'8' ]];then
+elif [[ x$project == x'8' ]]; then
     yum install -y python3 python3-yaml
     ln -s /usr/bin/python3 /usr/bin/python
     pip3 install click pandas numpy scipy
@@ -13,4 +25,6 @@ else
     echo "RHEL-$project is not supported!"
     exit 1
 fi
+
 echo "Setup finished!"
+exit 0
